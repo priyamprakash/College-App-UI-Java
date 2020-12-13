78ff5cef6ed9be32a4c575db5ad3c58bf703dd4f
Priyam Prakash <priyamprakash99@gmail.com>
2020-12-13 10:43:03 +0530

    AppInfo

--- MIT/app/src/main/java/com/mit_muzaffarpur/AppInfo.java
+++ MIT/app/src/main/java/com/mit_muzaffarpur/AppInfo.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AppInfo extends AppCompatActivity {
-
+//AppInfo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
